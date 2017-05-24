42b8c891e24852928aea65fd4a0b1a4b78c56ab0
Xing Zhou <xing@ohdyno.me>
2017-05-24 06:45:55 -0400

    Refactor tests to use FreeSpec instead of StringSpec.

--- src/test/kotlin/com/ford/labs/tdd/katas/calculator/CalculatorTest.kt
+++ src/test/kotlin/com/ford/labs/tdd/katas/calculator/CalculatorTest.kt
@@ -1,33 +1,33 @@
 package com.ford.labs.tdd.katas.calculator
 
 import io.kotlintest.matchers.shouldBe
-import io.kotlintest.specs.StringSpec
+import io.kotlintest.specs.FreeSpec
 
-
-class CalculatorTests : StringSpec({
+class CalculatorTests : FreeSpec({
     val calculator = Calculator()
 
-    "Given empty, returns 0" {
-        calculator.add("") shouldBe 0
-    }
+    "Add" - {
+        "0 numbers" - {
+            "should returns 0" {
+                calculator.add("") shouldBe 0
+            }
+        }
 
-    "Given 0, returns 0" {
-        calculator.add("0") shouldBe 0
-    }
+        "1 number" - {
+            "should return the input number" {
+                calculator.add("0") shouldBe 0
+                calculator.add("1") shouldBe 1
+                calculator.add("10") shouldBe 10
+                calculator.add("-12") shouldBe -12
+            }
+        }
 
-    "Given 1, return 1" {
-        calculator.add("1") shouldBe 1
-    }
-
-    "Given 0,1 return 1" {
-        calculator.add("0,1") shouldBe 1
-    }
-
-    "Given 1,1 return 2" {
-        calculator.add("1,1") shouldBe 2
-    }
-
-    "Given 10,1 return 11" {
-        calculator.add("10,1") shouldBe 11
+        "2 numbers" - {
+            "should return the sum of the numbers" {
+                calculator.add("0,1") shouldBe 1
+                calculator.add("1,1") shouldBe 2
+                calculator.add("10,1") shouldBe 11
+            }
+        }
     }
 })
